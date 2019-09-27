94d943363f3e7b28fdb2a76ee603f093ae99f5b3
a <test@com>
2019-09-27 16:54:54 +0900

    commit : starting commit.

---
+++ google-map-client-spring/src/main/java/com/kyiminhan/spring/GoogleMapClientSpringApplication.java
@@ -0,0 +1,29 @@
+package com.kyiminhan.spring;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+@SpringBootApplication
+public class GoogleMapClientSpringApplication {
+
+	public static void main(final String[] args) throws ApiException, InterruptedException, IOException {
+		SpringApplication.run(GoogleMapClientSpringApplication.class, args);
+
+		final GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyAlCRqMjnS5ztwm9Q5T9nThEaJ9OqoVnKI")
+				.build();
+		final GeocodingResult[] results = GeocodingApi.geocode(context, "東京都江東区大島").await();
+		final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		System.out.println(gson.toJson(results[0].addressComponents));
+
+	}
+
+}
